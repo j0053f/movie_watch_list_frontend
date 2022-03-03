b3c46d6663c5484089f5a59af80e72ba53f58ffd
yousef stdd <usf.stdd@gmail.com>
2022-03-03 17:22:18 +0330

    a fake login process tested!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from "./logo.svg";
+// import './App.css';
+import { useEffect, useState } from "react";
 function App() {
+  const [username, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  useEffect(() => {
+    const _username = "yousef";
+    const _password = "password_hash";
+
+    fetch("/api/movielist/v1.1/yousef", {
+      headers: {
+        Authorization: "Basic " + window.btoa(`${_username}:${_password}`),
+      },
+    })
+      .then((response) => console.log(response))
+      .catch((e) => console.log(e));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +32,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>{username ? `hello ${username}` : <LoginForm />}</h1>
+      <p></p>
     </div>
   );
 }
