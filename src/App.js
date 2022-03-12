7a8b439f6894f537387207e9042e02db222f2214
j0053f <j0003f@gmail.com>
2022-03-13 00:24:05 +0330

    move form login from Home to FormAddMovie

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
-import SignUp from './SignUp'
-import SignIn from './SignIn'
-import Home from './Home'
-import React, { useState } from 'react'
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+import Home from "./Home";
+import React, { useState } from "react";
 
-import { Route, Routes, Link } from 'react-router-dom'
+import { Route, Routes, Link } from "react-router-dom";
+import FormAddMovie from "./FormAddMovie";
 
 const u = {
   credentials: {},
-  setCredentials: () => {}
-}
-export const credentialsContext = React.createContext(u)
-function App () {
-  const [credentials, setCredentials] = useState({ username: '', password: '' })
+  setCredentials: () => {},
+};
+export const credentialsContext = React.createContext(u);
+function App() {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
 
   return (
     <div>
       <nav>
-        <Link to="/">Home</Link>|{'  '}
-        <Link to="/signin">Sign in</Link>|{'  '}
+        <Link to="/home">Home</Link>|{"  "}
+        <Link to="/signin">Sign in</Link>|{"  "}
         <Link to="/signup">Create an account</Link>
       </nav>
       <credentialsContext.Provider value={{ credentials, setCredentials }}>
-      <Routes>
-        <Route path="/">
-          <Route path="signup" element={<SignUp />} />
-          <Route path="signin" element={<SignIn />} />
-          <Route path='home' element ={<Home/>}/>
-        </Route>
-      </Routes>
+        <Routes>
+          <Route path="/">
+            <Route path="signup" element={<SignUp />} />
+            <Route path="signin" element={<SignIn />} />
+            <Route path="home" element={<Home />} />
+            <Route path="addmovie" element={<FormAddMovie />} />
+          </Route>
+        </Routes>
       </credentialsContext.Provider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/FormAddMovie.js
@@ -0,0 +1,108 @@
+import React, { useContext, useEffect, useState } from "react";
+import { credentialsContext } from "./App";
+import { Link } from "react-router-dom";
+
+export default function FormAddMovie() {
+  const { username, password } = useContext(credentialsContext).credentials;
+  const [movieName, setMovieName] = useState("");
+  const [seasonEpisodeCount, setSeasonEpisodeCount] = useState([]);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function handleChange(event) {
+    const { target } = event;
+    target.name === "movieName" && setMovieName(target.value);
+    if (target.name === "numSeason") {
+      setSeasonEpisodeCount(new Array(+target.value).fill(0));
+    }
+  }
+
+  function handleSeasonEpisodeCount(seasonNo) {
+    const arr = seasonEpisodeCount.slice();
+    return function handleEpisodeCount(episodeCount) {
+      arr[seasonNo] = episodeCount;
+      setSeasonEpisodeCount(arr);
+    };
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(seasonEpisodeCount);
+    setIsSubmitted(true);
+  }
+
+  useEffect(() => {
+    if (isSubmitted) {
+      fetch("/api/movielist/v1.1/addmovie", {
+        method: "POST",
+        headers: {
+          Authorization: `Basic ${window.btoa(`${username}:${password}`)}`,
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          details: seasonEpisodeCount,
+          name: movieName,
+        }),
+      }).then((response) => console.log(response));
+      setIsSubmitted(false);
+    }
+  });
+
+  return (
+    <div>
+      <h1>hello {username}</h1>
+      <Link to="/home"> {"<-"}</Link>
+      <form onSubmit={handleSubmit}>
+        <label>
+          movie name:
+          <input
+            type="text"
+            name="movieName"
+            value={movieName}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          number of season:
+          <input
+            type="number"
+            name="numSeason"
+            value={seasonEpisodeCount.length}
+            onChange={handleChange}
+          />
+        </label>
+        <div>
+          {seasonEpisodeCount.map((element, index) => (
+            <Episode
+              key={index}
+              index={index}
+              _value={element}
+              handleEpisodeCount={handleSeasonEpisodeCount(index)}
+            />
+          ))}
+        </div>
+
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
+}
+
+function Episode({ index, _value, handleEpisodeCount }) {
+  function handleChange(event) {
+    const { target } = event;
+    if (target.name === "numEpisode") handleEpisodeCount(+target.value);
+  }
+  return (
+    <div>
+      <label>
+        {index + 1}:
+        <input
+          type="number"
+          name="numEpisode"
+          value={_value}
+          onChange={handleChange}
+        />
+      </label>
+    </div>
+  );
+}
